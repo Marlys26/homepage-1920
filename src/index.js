4b41c60c55287d381f1efd130d6abe8c650140f4
marlys26 <marlysrc@gmail.com>
2022-02-25 19:20:10 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// Modal function //
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+//Onload animation
+window.onload = function () {
+  setInterval(function () {
+    $(".animate").addClass("active");
+  }, 1000);
+};
+
+// Scroll animation
+$(function () {
+  var header = $(".animate_scroll");
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 200) {
+      header.addClass("active");
+    } else {
+      header.removeClass("active");
+    }
+  });
+});
